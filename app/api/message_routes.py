d208e1194e724a953b4fe5052849d34cb4287640
Tre <trburrows@syr.edu>
2024-12-30 21:45:24 -0500

    messages and rsvp routes start

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -5,3 +5,15 @@ from flask_login import current_user, login_required
 import datetime
 
 message_routes = Blueprint('messages', __name__)
+
+@message_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def edit_messages():
+
+@message_routes.route('/', methods=['POST'])
+@login_required
+def create_message():
+
+@message_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_message():
--- app/api/request_routes.py
+++ app/api/request_routes.py
@@ -5,3 +5,15 @@ from flask_login import current_user, login_required
 import datetime
 
 request_routes = Blueprint('requests', __name__)
+
+@request_routes.route('/')
+@login_required
+
+@request_routes.route('/', methods=[''])
+@login_required
+
+@request_routes.route('/', methods=[''])
+@login_required
+
+@request_routes.route('/', methods=[''])
+@login_required
