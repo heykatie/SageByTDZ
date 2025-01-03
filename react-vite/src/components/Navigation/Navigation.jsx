9263288ed64f184d764de42fb8a118607f8ab01a
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-01-03 15:05:11 -0800

    create group button in navigation only appears when logged in - Files: react-vite/src/components/Navigation/Navigation.jsx

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -2,18 +2,22 @@ import { NavLink } from 'react-router-dom';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 import { useState } from 'react';
+import { useSelector } from 'react-redux'; // Import to get the session user
 import CreateGroupModal from '../CreateGroupModal';
 
 function Navigation() {
 	const [showCreateGroupModal, setShowCreateGroupModal] = useState(false);
 
+	// Get the logged-in user from Redux store
+	const user = useSelector((state) => state.session.user);
+
 	// Open modal
-	const openModal = () => {
+	const openGroupModal = () => {
 		setShowCreateGroupModal(true);
 	};
 
 	// Close modal
-	const closeModal = () => {
+	const closeGroupModal = () => {
 		console.log('Closing modal'); // Debug log to ensure it's firing
 		setShowCreateGroupModal(false);
 	};
@@ -29,15 +33,19 @@ function Navigation() {
 					<ProfileButton />
 				</li>
 
-				{/* Create Group Modal */}
-				<li className='create-group-container'>
-					<button className='create-group-button' onClick={openModal}>
-						Create Group
-					</button>
-					{showCreateGroupModal && (
-						<CreateGroupModal onClose={closeModal} />
-					)}
-				</li>
+				{/* Show "Create a Group" button only if user is logged in */}
+				{user && (
+					<li className='create-group-container'>
+						<button
+							className='create-group-button'
+							onClick={openGroupModal}>
+							Create a Group
+						</button>
+						{showCreateGroupModal && (
+							<CreateGroupModal onClose={closeGroupModal} />
+						)}
+					</li>
+				)}
 			</ul>
 		</nav>
 	);
