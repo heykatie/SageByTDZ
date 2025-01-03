bcb7eda72fe7d8d958c82b911eb0b34b757b6d9a
Nia C <157457101+nlclark216@users.noreply.github.com>
2025-01-02 20:47:47 -0500

    friend reducer working

--- react-vite/src/components/Friends/Friends.jsx
+++ react-vite/src/components/Friends/Friends.jsx
@@ -1,8 +1,30 @@
 import './Friends.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as friendActions from '../../redux/friends';
 
 function Friends() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(friendActions.thunkAllFriends())
+    }, [dispatch])
+
+    const friends = useSelector(state=>state.friends.allFriends);
+    let friendsArr;
+
+    if(friends) {
+        friendsArr = Object.values({...friends})
+    }
+
     return (
-        <h1>Friends</h1>
+        <div>
+            {friendsArr && friendsArr.map(friend=>(
+                <div key={friend.id}>
+                    <span>{friend.first_name}</span>
+                </div>
+            ))}
+        </div>
     )
 }
 
