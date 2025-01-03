34c9823e9def84b746f8c6e6da382943c4f14b06
Kolten <kolten.smith1@gmail.com>
2025-01-03 13:01:38 -0800

    Invite form spelling fix

---
+++ react-vite/src/components/InvitesFormPage/InvitesFormPage.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+
+
+
+
+function InviteFormPage() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    
+    const [friend, setFriend] = useState();
+}
